cd1f783bcbbafae98afffe0427015fd6f2e288fd
Robin <robinli@live.ca>
2016-05-06 11:16:29 -0400

    V0.4 Loop and return

--- PokerGame.py
+++ PokerGame.py
@@ -13,12 +13,13 @@ V0: introduce Module_Basic
 import Module_Basic
 import Module_5CardStud
 
-global deck
-
 def menu_display():
     print ("           Welcome to play the Card Games ! \n")
 
 def main():
+## Initial Setting
+    value_msg={0:"High Card",1:"One Pair",2:"Two Pair",3:"3 of A Kind",4:"Straight",5:"Flush",6:"Full House",7:"4 of A Kind",8:"Straight Flush"}
+
 # initialize a deck
     deck=[]
     Hand_A=[]
@@ -37,7 +38,6 @@ def main():
     ##print (len(deck)," cards in the deck ",deck," now.\n\n")
     Module_Basic.ShuffleDeck(deck)
 
-    
     menu_display()
     exit_index="a"
     while (exit_index!="E"):
@@ -48,52 +48,49 @@ def main():
         Module_Basic.DrawCard(deck,Hand_B)
 # Door card and first betting round
         Module_Basic.DrawCard(deck,Hand_A)
-        print ("Player A has: ",Hand_A[1:],"\n")
+        print ("Player A has: ",Hand_A[1:])
         Module_Basic.DrawCard(deck,Hand_B)
         print ("Player B has: ",Hand_B[1:],"\n")
 #Actions: Fold, Call, Raise
 #3rd street and second betting round
         Module_Basic.DrawCard(deck,Hand_A)
-        print ("Player A has: ",Hand_A[1:],"\n")
+        print ("Player A has: ",Hand_A[1:])
         Module_Basic.DrawCard(deck,Hand_B)
         print ("Player B has: ",Hand_B[1:],"\n")
 #Actions: Fold, Call, Raise
 #4th street and third betting round
         Module_Basic.DrawCard(deck,Hand_A)
-        print ("Player A has: ",Hand_A[1:],"\n")
+        print ("Player A has: ",Hand_A[1:])
         Module_Basic.DrawCard(deck,Hand_B)
         print ("Player B has: ",Hand_B[1:],"\n")
 #Actions: Fold, Call, Raise
 #5th street and fourth betting round
         Module_Basic.DrawCard(deck,Hand_A)
-        print ("Player A has: ",Hand_A[1:],"\n")
+        print ("Player A has: ",Hand_A[1:])
         Module_Basic.DrawCard(deck,Hand_B)
         print ("Player B has: ",Hand_B[1:],"\n")
 #Actions: Fold, Call, Raise
 
 ###Test Hand
 ##      Hand_A=["AS","3S","2S","5S","4S"]
-##      Hand_B=["AC","3C","2C","5C","4C"]
+##      Hand_B=["6H","3D","9S","3C","9H"]
 
 # Sort all hands before comparing
         Sorted_Hand_A=sorted(Hand_A, key=value_dict.get)
         Sorted_Hand_B=sorted(Hand_B, key=value_dict.get)
-        print (Sorted_Hand_A,"\n")
-        print (Sorted_Hand_B,"\n")
 
 # Showdown
         Hand_A_value=Module_5CardStud.EvaluateHand(Sorted_Hand_A)
         Hand_B_value=Module_5CardStud.EvaluateHand(Sorted_Hand_B)
-        print ("A: ",Hand_A," vs B:",Hand_B, "\n")
+        print ("A: ",Sorted_Hand_A," ",value_msg[Hand_A_value[0]], "  vs  ")
+        print ("B:",Sorted_Hand_B," ",value_msg[Hand_B_value[0]])
         if (Hand_A_value[0]==Hand_B_value[0]):
-            print ("Both got ", Hand_A_value[2]," but ")
             print ("A: ",name_dict[Hand_A_value[1]],"vs B: ",name_dict[Hand_B_value[1]],"\n")
             if(value_dict[Hand_A_value[1]]>value_dict[Hand_B_value[1]]):
                 print ("A wins. \n\n")
             else:
                 print ("B Wins. \n\n")
         else:
-            print ("A ",Hand_A_value[2],"vs B ",Hand_B_value[2],"\n")
             if (Hand_A_value[0]>Hand_B_value[0]):
                 print ("A Wins. \n\n")
             else:
